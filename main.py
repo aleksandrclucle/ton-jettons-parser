b7e5daf0a5cba55a05988a6c0ee46412b43525ba
aleksandrclucle <aleksandrclucle@gmail.com>
2024-04-15 10:11:00 +0300

    add contract id

--- main.py
+++ main.py
@@ -9,6 +9,13 @@ headers = {
     'accept': 'application/json'
 }
 
+
+def check_and_strip_prefix(s):
+    if s.startswith("ton_"):
+        return s[4:]
+    return s
+
+
 for page in range(1, 11):
     url = f'https://api.geckoterminal.com/api/v2/networks/ton/new_pools?page={page}'
     response = requests.get(url, headers=headers)
@@ -17,11 +24,13 @@ for page in range(1, 11):
     for item in data['data']:
         pool_name = item["attributes"]["name"]
         dex_id = item["relationships"]["dex"]["data"]["id"]
+        contract = item["relationships"]["base_token"]["data"]["id"]
+        contract = check_and_strip_prefix(contract)
         pool_created_at = item["attributes"]["pool_created_at"]
         readable_date = datetime.fromisoformat(pool_created_at.replace('Z', '+00:00')).strftime('%Y-%m-%d %H:%M:%S')
         reserve_in_usd = int(float(item["attributes"]["reserve_in_usd"]))
         if reserve_in_usd > user_liq:
-            result.append((pool_name, dex_id, readable_date, reserve_in_usd))
+            result.append((pool_name, dex_id, readable_date, reserve_in_usd, contract))
 
-for name, dex, readable_date, reserve_in_usd_int in result:
-    print(f'Jetton: {name}, DEX: {dex}, Date: {readable_date}, Liquidity: {reserve_in_usd_int}')
+for name, dex, readable_date, reserve_in_usd, contract in result:
+    print(f'Jetton: {name}, DEX: {dex}, Date: {readable_date}, Liquidity: {reserve_in_usd}, contract: {contract}')
